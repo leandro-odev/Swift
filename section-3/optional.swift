6d868c7a642a7d33e92a9c7e8a5f3839dee29fc0
Leandro <devleandromcz@gmail.com>
2024-03-16 01:00:12 -0300

    Refactor optional handling and add examples of force unwrapping, nil-coalescing, and optional chaining

--- section-3/optional.swift
+++ section-3/optional.swift
@@ -10,7 +10,7 @@ let possibleNumber2: Int? = nil
 if let number = possibleNumber2 {
     print("The number is \(number)")
 } else {
-    print("possibleNumber2 is nil")
+    //print("possibleNumber2 is nil")
 }
 
 func printNumber(_ possiblex: Int?) {
@@ -21,4 +21,33 @@ func printNumber(_ possiblex: Int?) {
     print("The number is \(numberx)")
 }
 let possible: Int? = 42
-printNumber(possible)
+//printNumber(possible)
+
+// Force unwrapping
+var possibleNumber3: Int? = 42
+let number = possibleNumber3! // Yay ✅ when possibleNumber3 is not nil, otherwise crash ❌
+
+// Nil-Coalescing
+let nickName: String? = "John"
+let defaultName: String = "Nick"
+let informalGreeting = "Hi \(nickName ?? defaultName)"
+//print(informalGreeting)
+
+//Optional Chaining
+class Person {
+    var residence: Residence?
+}
+class Residence {
+    var numberOfRooms = 1
+}
+
+let john = Person()
+//let roomCount = john.residence!.numberOfRooms // ❌
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    // this else will be executed, because john.residence is nil
+    //print("Unable to retrieve the number of rooms.")
+}
+
+
